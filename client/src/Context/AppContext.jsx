8ad0b8690659bab8921b8512835111f3f9f5e1f2
Hamza Sheikh <zafarhamza789@gmail.com>
2025-01-20 18:22:21 +0500

    loginError

--- client/src/Context/AppContext.jsx
+++ client/src/Context/AppContext.jsx
@@ -1,15 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
+
+    // const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = "http://localhost:4000";
+    const [isLoggedin, setIsLoggedin] = useState(false);
+    const [userData, setUserData] = useState(false);
+
     const value = {
-        
+        backendUrl,
+        isLoggedin,
+        setIsLoggedin,
+        userData,
+        setUserData,
+
     }
     return (
-        <AppContext.Provider value={{...props }}>
+        <AppContext.Provider value={{value}}>
             {props.children}
         </AppContext.Provider>
     );
--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,15 +1,57 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../Context/AppContext";
+import axios from 'axios'
+import { toast } from "react-toastify";
 
 const Login = () => {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const { backendUrl, setIsLoggedin } = useContext(AppContext)
 
   const [state, setState] = useState('Sign Up');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const onSubmithandler = async (e) => {
+
+    try {
+      e.preventDefault();
+      axios.defaults.withCredentials = true;
+
+      if (state === 'Sign Up') {
+        const { data } = await axios.post(`${backendUrl}/api/auth/register`, {name, email, password });
+
+
+        if (data.success) {
+          setIsLoggedin(true);
+          navigate('/');
+        } else {
+          toast.error(data.message);
+        }
+      }
+      else {
+        const { data } = await axios.post(`${backendUrl}/api/auth/login`, { email, password });
+
+        if (data.success) {
+          setIsLoggedin(true);
+          navigate('/');
+        } else {
+          toast.error(data.message);
+        }
+      }
+
+    } catch (error) {
+      toast.error(error.message);
+    }
+
+
+  }
+
+
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
       <img onClick={() => navigate('/')} src={assets.logo} className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer" />
@@ -17,7 +59,7 @@ const Login = () => {
       <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
         <h2 className="text-3xl font-semibold text-white text-center mb-3"> {state === 'Sign Up' ? 'Create Account' : 'Login'} </h2>
         <p className="text-center text-sm mb-6">{state === 'Sign Up' ? 'Create your Account' : 'Login your Account'}</p>
-        <form >
+        <form onSubmit={onSubmithandler}>
 
           {state === "Sign Up" && (
             <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
@@ -25,9 +67,6 @@ const Login = () => {
               <input onChange={e => setName(e.target.value)} value={name} className="bg-transparent outline-none text-white" type="text" name="name" placeholder="Full Name" id="" required />
             </div>
           )}
-
-
-
           <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.mail_icon} alt="" />
             <input onChange={e => setEmail(e.target.value)} value={email} className="bg-transparent outline-none text-white" type="email" name="email" placeholder="Enter Email" id="" required />
