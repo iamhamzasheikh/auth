e4e02cc7804c22bff7f7a15b4e45daab6ea002d5
Hamza Zafar <zafarhamza789@gmail.com>
2025-10-06 14:13:30 +0500

    updated2.7

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -96,7 +96,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
         });
 
@@ -117,7 +117,7 @@ export const logout = async (req, res) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         });
 
         console.log('Cookie cleared successfully', req.cookies.token);
