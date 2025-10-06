765d04062aa32dc5215180d4ca1350f8d23c7baa
Hamza Zafar <zafarhamza789@gmail.com>
2025-10-06 14:05:15 +0500

    updated2.5

--- server/server.js
+++ server/server.js
@@ -15,29 +15,9 @@ connectDB();
 
 // const allowedOrigins = ['http://localhost:5173']
 
-const allowedOrigins = [
-    'http://localhost:5173',                // for local development (Vite)
-    'https://auth-q44fuos0c-iamhamzasheikhs-projects.vercel.app'      // 🔹 replace with your deployed frontend URL
-];
-
-
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({ origin: true, credentials: true }));
-
-
-// 🔹 4. Configure CORS BEFORE routes
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
-app.use(cors({
-    origin: allowedOrigins,
-    credentials: true, // allow cookies to be sent cross-site
-}));
+app.use(cors({ origin: true, credentials: true }));
 
 //api endpoints
 app.get('/', (req, res) => res.send('Server is Running'));
